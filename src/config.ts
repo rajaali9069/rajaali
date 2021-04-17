9efc575f26e33f1e84bdcc1b96f200250ef6bb6d
DESKTOP-JOITHU0\mehdi <rajaali.andpercent@gmail.com>
2021-04-17 17:32:28 +0500

    changes

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,10 @@ export const appConfig =
 {
     // // Staging
     
-    baseUrl: ' http://151.80.119.82:9004/api/v1/',
-    file_url: 'http://151.80.119.82:8000/',
+    baseUrl: 'http://www.creativeideate.com/ecommerce/api/v1/',
+    file_url: 'http://www.creativeideate.com/uploads/',
+    // baseUrl: 'http://151.80.119.82:9004/api/v1/',
+    // file_url: 'http://151.80.119.82:8000/',
     // file_url_vendors: 'http://13.126.174.129/uploads/vendors/',
 
 }
